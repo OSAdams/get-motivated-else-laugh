9e3253d79ae0b282372f862a321bd586329b3c6e
Owen Adams <owenadams.main@gmail.com>
2021-02-23 18:22:20 -0500

    Added function to pull random quotes from API. #2

--- js/main.js
+++ js/main.js
@@ -11,12 +11,28 @@ var $textContainer = document.querySelector('#quotes-or-jokes')
 
 $motivateButton.addEventListener('click', function(event) {
   $logo.setAttribute('class', 'logo hidden');
-  gsap.from($textContainer, {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
+  // if () {
+    gsap.from($textContainer, {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
+    // }
   $textContainer.setAttribute('class', 'quotes-or-jokes');
   $jokeId.setAttribute('class', 'jokes hidden');
   $quoteId.setAttribute('class', 'quotes');
+  getQuote();
 });
 
-// function getQuote () {
-
-// };
+function getQuote () {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://type.fit/api/quotes");
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    var randomNum = Math.floor(Math.random() * 1643);
+    var $response = xhr.response[randomNum];
+    var $liText = document.createElement('li');
+    $liText.textContent = $response.text;
+    $quoteId.appendChild($liText);
+    var $liAuthor = document.createElement('li');
+    $liAuthor.textContent = $response.author;
+    $quoteId.appendChild($liAuthor);
+    });
+  xhr.send();
+};
