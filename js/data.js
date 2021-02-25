3a89722dfde5648792a632d669b4709295c1a2aa
Owen Adams <owenadams.main@gmail.com>
2021-02-24 20:42:51 -0500

    Fixed functionality to store values in localStorage. #4

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
 /* exported data */
 var quoteData = [];
+var quoteStorage = localStorage.getItem('motivate-or-laugh');
+
+if (quoteStorage !== null) {
+  quoteData = JSON.parse(quoteStorage);
+}
 
 window.addEventListener('beforeunload', function (event) {
   var quoteJSON = JSON.stringify(quoteData);
