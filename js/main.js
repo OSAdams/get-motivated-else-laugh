0813f1e19b22a58131431af093e8187de1541e13
Owen Adams <owenadams.main@gmail.com>
2021-03-02 14:09:21 -0500

    Removed some code for animations. #13

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 gsap.from('.logo-image', {duration: 1.5, opacity: 0, scale: 0.3, ease: 'back'});
-gsap.from('.motivate-quotes', {duration: 0.75, opacity: 0, x: -250});
-gsap.from('.dad-jokes', {duration: 1, opacity: 0, x: -250});
 gsap.from('.fas', {duration: 1.1, opacity: 0, y: 150, stagger: 0.25});
 
 var $motivateButton = document.querySelector('#motivate-quotes');
